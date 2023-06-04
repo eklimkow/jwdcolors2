5233781dca1ed2ca5dea6add106cc6ebe6675360
eklimkow <ewamewatestuje@gmail.com>
2023-06-04 16:04:35 +0200

    layout aplikacji

---
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/DataController.java
@@ -0,0 +1,28 @@
+package pl.edu.wszib.jwd.jwdcolors.controller;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.thymeleaf.util.StringUtils;
+import pl.edu.wszib.jwd.jwdcolors.service.SelectedColorService;
+
+@Controller
+public class DataController {
+
+    @Value("${app.title.data}")
+    private String title;
+
+    @Autowired
+    SelectedColorService selectedColorService;
+
+    @GetMapping("/data")
+    String dataPage(Model model) {
+        model.addAttribute("title", title);
+        model.addAttribute("selectedColors", selectedColorService.getAllData());
+        return "data";
+    }
+
+}
--- src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/SelectedColorController.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/SelectedColorController.java
@@ -9,6 +9,7 @@ import org.springframework.web.bind.annotation.PathVariable;
 import org.thymeleaf.util.StringUtils;
 import pl.edu.wszib.jwd.jwdcolors.dao.SelectedColorDao;
 import pl.edu.wszib.jwd.jwdcolors.model.SelectedColor;
+import pl.edu.wszib.jwd.jwdcolors.service.SelectedColorService;
 
 import java.util.Date;
 
@@ -19,12 +20,14 @@ public class SelectedColorController {
     private String title;
 
     @Autowired
-    SelectedColorDao selectedColorDao;
+    SelectedColorService selectedColorService;
+
 
     @GetMapping({"/select", "/select/{color}"})
-    public String selectedColorPage(@PathVariable(required = false) String color, Model model) {
-        if (!StringUtils.isEmpty(color)){
-            selectedColorDao.save(new SelectedColor(color, new Date()));
+    public String selectedColorPage(@PathVariable(required = false) String color,  Model model) {
+
+        if (!StringUtils.isEmpty(color)) {
+            selectedColorService.save(color);
         }
 
         String[][] colors = {
@@ -33,9 +36,14 @@ public class SelectedColorController {
                 {"gray","silver", "olive", "lime"},
                 {"navy","white", "aqua", "fuchsia"}
         };
+
         model.addAttribute("title", title);
         model.addAttribute("colors", colors);
         return "select";
     }
 
+    @GetMapping("/")
+    public String showIndex() {
+        return "redirect:select";
+    }
 }
