b029d6e2f716d782644f475d332ec190f8fc742b
eklimkow <ewamewatestuje@gmail.com>
2023-06-04 11:52:08 +0200

    testy dao

--- src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/HelloController.java
+++ src/main/java/pl/edu/wszib/jwd/jwdcolors/controller/HelloController.java
@@ -1,4 +1,4 @@
-package pl.edu.wszib.jwd.jwdcolors.Controller;
+package pl.edu.wszib.jwd.jwdcolors.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RestController;
